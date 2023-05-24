d8e2df88eb17572cccaed7f18a1d0d9fc5f64c60
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-24 16:53:51 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            This is add coffee page
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            This is update coffee page in this website.
+        </div>
+    );
+};
+
+export default UpdateCoffee;
